de75889a12139e00cb1a4d57cdbef73485d1862a
Carina Sweet <carina.sweet06@gmail.com>
2021-07-07 17:19:12 -0600

    add child index test

--- app/controllers/vegetables_controller.rb
+++ app/controllers/vegetables_controller.rb
@@ -2,4 +2,9 @@ class VegetablesController < ApplicationController
   def index
     @vegetables = Vegetable.all
   end
+
+  def show
+    @vegetable = Vegetable.find(params[:id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
   get '/', to: 'welcome#index'
+
+  get '/vegetables', to: 'vegetables#index'
+
+  get '/vegetables/:id', to: 'vegetables#show'
 end
