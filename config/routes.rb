d38e05507f6de1449956123feba1ef712452ceaa
Carina Sweet <carina.sweet06@gmail.com>
2021-07-07 21:30:30 -0600

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/', to: 'welcome#index'
 
+  get '/vegetable_gardens', to: 'vegetable_gardens#index'
+
+  get '/vegetable_gardens/:id', to: 'vegetable_gardens#show'
+
   get '/vegetables', to: 'vegetables#index'
 
   get '/vegetables/:id', to: 'vegetables#show'
+
+  get '/vegetable_gardens/:vegetable_garden_id/vegetables', to: 'vegetable_gardens#vegetables_index'
 end
