042f57749cb9f03374df1a6ae14881e5e44b3760
Carina Sweet <carina.sweet06@gmail.com>
2021-07-08 20:31:55 -0600

    Update models tests

--- spec/models/vegetable_garden_spec.rb
+++ spec/models/vegetable_garden_spec.rb
@@ -1,7 +1,5 @@
 require 'rails_helper'
 
 describe VegetableGarden do
-  describe 'relationships' do
-    it {should have_many(:vegetables)}
-  end
+  it {should have_many(:vegetables)}
 end
--- spec/models/vegetable_spec.rb
+++ spec/models/vegetable_spec.rb
@@ -2,6 +2,6 @@ require 'rails_helper'
 
 describe Vegetable do
   describe 'validations' do
-    it {should belong_to(:vegetable_garden)}
+    it {should belongs_to(:vegetable_garden)}
   end
 end
