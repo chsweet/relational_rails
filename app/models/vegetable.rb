29c4854c4ffd9f664c43ff461f89305ed08bfddc
Carina Sweet <carina.sweet06@gmail.com>
2021-07-13 11:36:20 -0600

    Update test for perennial vegetable and sort alphabetical

--- app/models/vegetable.rb
+++ app/models/vegetable.rb
@@ -2,6 +2,11 @@ class Vegetable < ApplicationRecord
   belongs_to :garden
 
   def self.perennial_vegetable
-    self.where(perennial: true)
+    where(perennial: true)
+  end
+
+#add downcase?? 
+  def self.sort_alphabetical
+    order(:name)
   end
 end
--- spec/models/vegetable_spec.rb
+++ spec/models/vegetable_spec.rb
@@ -5,7 +5,7 @@ describe Vegetable do
     it {should belong_to(:garden)}
   end
 
-  describe 'instance method' do
+  describe 'class method' do
     before :each do
       @garden_1 = Garden.create!(created_at: '03-05-2021', name: 'Sweet Garden', weeded: true, hrs_of_sun: 15)
       @tomato_1 = @garden_1.vegetables.create!(name: 'Cherokee Purple', perennial: true, min_hrs_sun: 8)
@@ -13,17 +13,16 @@ describe Vegetable do
       @onion_1 = @garden_1.vegetables.create!(name: 'Sweet Yellow Onion', perennial: true, min_hrs_sun: 14)
     end
 
-    # describe '#perennial_vegetables' do
-    #   it 'filters only perennial vegetables'do
-    #     expect(@onion_1.perennial_vegetable).to eq(true)
-    #   end
-    # end
+    describe '#perennial_vegetables' do
+      it 'filters only perennial vegetables'do
+        expect(Vegetable.perennial_vegetable).to eq([@tomato_1, @onion_1])
+      end
+    end
 
-    # describe '#sort_vegetables' do
-    #   xit 'sorts vegetables alphabeticaly' do
-    #     expect(@tomato_1.name).to appear_before(@potato_1.name, only_text: true)
-    #     expect(@potato_1.name).to_not appear_before(@onion_1.name, only_text: true)
-    #   end
-    # end
+    describe '#sort_vegetables' do
+      it 'sorts vegetables alphabeticaly' do
+        expect(Vegetable.sort_alphabetical).to eq([@tomato_1, @onion_1, @potato_1])
+      end
+    end
   end
 end
